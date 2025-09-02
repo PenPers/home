fbda665b1a2d1a67f31653e85f110d4b47c0d58e
PenPers <mohamadmahdi13895@gmail.com>
2025-09-02 08:08:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,7 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000)
